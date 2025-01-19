6e31a7effcb06f1bede5a8f69d0124790f3f2e1f
Hanielle <saphielle.akiyama@gmail.com>
2025-01-20 02:43:28 +0900

    Initial client

---
+++ client/main.c
@@ -0,0 +1,61 @@
+// client.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    // Create socket
+    int client_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_sock == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure server address
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr) <= 0) {
+        perror("inet_pton");
+        close(client_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to server
+    if (connect(client_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("connect");
+        close(client_sock);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to server\n");
+
+    for (int i = 0; i < 3; i++) {
+        // Send message to server
+        char buffer[BUFFER_SIZE];
+        snprintf(buffer, BUFFER_SIZE, "Message %d from client", i + 1);
+        if (send(client_sock, buffer, strlen(buffer), 0) == -1) {
+            perror("send");
+            close(client_sock);
+            exit(EXIT_FAILURE);
+        }
+
+        // Receive response from server
+        memset(buffer, 0, BUFFER_SIZE);
+        if (recv(client_sock, buffer, BUFFER_SIZE, 0) <= 0) {
+            perror("recv");
+            close(client_sock);
+            exit(EXIT_FAILURE);
+        }
+        printf("Server: %s\n", buffer);
+    }
+
+    close(client_sock);
+    return 0;
+}
+
